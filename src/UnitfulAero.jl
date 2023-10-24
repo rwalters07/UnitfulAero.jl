3401387ee8d270c9e00bb9f2a1a1d41584f3c180
rwalters07 <robert.walters07@gmail.com>
2023-10-23 21:44:24 -0400

    added aero and thermofluid units

--- src/UnitfulAero.jl
+++ src/UnitfulAero.jl
@@ -1,5 +1,11 @@
 module UnitfulAero
-
-# Write your package code here.
+    using Unitful; 
+	@unit nmi "nautical mile" NautMile 1852u"m" false;
+	# https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication330e2008.pdf 
+	Unitful.register(UnitfulAero);
+	@unit kt     "kt"    Knot        1u"nmi/hr" 				false;
+	@unit hp     "hp"    HorsePower  550u"lbf*ft/s" 			false;
+	@unit lbm    "lbm"   Poundmass   1u"lbf"/32.174u"ft/s^2" 	false;
+	@unit ton    "ton"   Ton         200u"btu/minute" 			false;
 
 end
