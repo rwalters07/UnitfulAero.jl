0327d2751140e1cc3d9d58903fb26a62619e08f9
rwalters07 <133813583+rwalters07@users.noreply.github.com>
2023-10-24 14:17:28 -0400

    Update UnitfulAero.jl commented out register

--- src/UnitfulAero.jl
+++ src/UnitfulAero.jl
@@ -2,7 +2,7 @@ module UnitfulAero
     using Unitful
 	@unit nmi "nautical mile" NautMile 1852u"m" false;
 	# https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication330e2008.pdf 
-	Unitful.register(UnitfulAero);
+	#Unitful.register(UnitfulAero);
 	@unit kt     "kt"    Knot        1u"nmi/hr" 				false;
 	@unit hp     "hp"    HorsePower  550u"lbf*ft/s" 			false;
 	@unit lbm    "lbm"   Poundmass   1u"lbf"/32.174u"ft/s^2" 	false;
